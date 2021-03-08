cfb09b2e0888698d14b2754a965594d1bb9770bc
George Matheos <georgematheos@gmail.com>
2021-03-08 11:07:00 -0500

    remove unused

--- main/unfinished/cpt_sampler/abstract.jl
+++
@@ -1,7 +0,0 @@
-struct AbstractCPTSampler{C} <: GenericComponent
-    cpt::C
-    AbstractCPTSampler(cpt::C) where {C <: CPT} = new{C}(cpt)
-end
-# performance TODO: pass in iterator, not tuple?
-inputs(s::AbstractCPTSampler) = CompositeValue(Tuple(FiniteDomainValue(n) for n in s.cpt.input_domain_sizes))
-outputs(s::AbstractCPTSampler) = CompositeValue((FiniteDomainValue(s.cpt.output_domain_size),))
