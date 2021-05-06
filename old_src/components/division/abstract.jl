93f17a841ac1f114038ef739e7e82081451ea351
George Matheos <georgematheos@gmail.com>
2021-05-06 10:44:57 -0400

    delete `old_src` folder

--- old_src/components/division/abstract.jl
+++
@@ -1,9 +0,0 @@
-"""
-    PositiveRealDivider <: GenericComponent
-    PositiveRealDivider()
-
-Divides PositiveReal input `:numerator` by PositiveReal input `:denominator`.
-"""
-struct PositiveRealDivider <: GenericComponent end
-Circuits.inputs(::PositiveRealDivider) = NamedValues(:numerator => PositiveReal(), :denominator => PositiveReal())
-Circuits.outputs(::PositiveRealDivider) = NamedValues(:out => PositiveReal())
