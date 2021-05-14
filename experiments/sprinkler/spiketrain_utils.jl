d51154c3096631e51d3bfbc1043c34ddfe6dfcb6
George Matheos <georgematheos@gmail.com>
2021-05-13 21:37:31 -0400

    bug fix in mh spiketrain viz

--- experiments/sprinkler/spiketrain_utils.jl
+++ experiments/sprinkler/spiketrain_utils.jl
@@ -29,20 +29,29 @@ end
 
 function get_names_and_trains(dict)
     mh_sample_indices = Set(key[1] for key in keys(dict))
-    valnames = Set(key[2] for key in keys(dict) if key[1] == first(mh_sample_indices))
+    @assert all((i in mh_sample_indices) for i=1:length(mh_sample_indices))
+    mh_sample_indices = 1:length(mh_sample_indices)
+    valnames = Set(key[2] for key in keys(dict))
     vals = Dict(
-        valname => Set(key[3] for key in keys(dict)
-        if key[1] == first(mh_sample_indices) && key[2] == valname)
+        valname => Set(
+            key[3] for key in keys(dict)
+            if  key[2] == valname
+        )
         for valname in valnames
     )
     names = []
     spiketrains = []
+    display(dict)
     for idx=1:length(mh_sample_indices)
         @assert idx in mh_sample_indices
         for valname in valnames
             for val in vals[valname]
-                push!(names, "$idx | $valname = $val")
-                push!(spiketrains, dict[(idx, valname, val)])
+                if haskey(dict, (idx, valname, val))
+                    push!(names, "$idx | $valname = $val")
+                    push!(spiketrains, dict[(idx, valname, val)])
+                else
+                    println("Could not find key $((idx, valname, val))")
+                end
             end
         end
     end
