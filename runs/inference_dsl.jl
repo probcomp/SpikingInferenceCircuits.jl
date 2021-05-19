9fe3b18ec184ab5f3cfe3fa9058ec123eb1d0743
McCoy Becker <mccoybecker@gmail.com>
2021-05-18 23:56:13 -0400

    Initial version of DSL.

--- runs/inference_dsl.jl
+++ runs/inference_dsl.jl
@@ -43,6 +43,14 @@ end
 
 _lift(::Type{Bool}) = EnumeratedDomain([true, false])
 _lift(r::UnitRange{Int64}) = EnumeratedDomain(r)
+function _concrete(r::EnumeratedDomain{Vector{Bool}})
+    @assert(length(r.vals) == 2)
+    return FiniteDomain(1)
+end
+function _concrete(r::EnumeratedDomain{<:UnitRange})
+    v = r.vals
+    return FiniteDomain(v.stop - v.start)
+end
 
 function striptypes(ex::Expr)
     ex.head == :(::) || return ex
@@ -61,17 +69,65 @@ macro compile(expr)
     esc(ex)
 end
 
-b = @compile object_motion_step(1:10)
-display(b)
+m = @compile object_motion_step(1:10)
+sp = @compile step_proposal(1:10, 1:10)
+
+function mh(m::InferenceBundle, p::InferenceBundle)
+    return MHKernel(m.genfn, Tuple(_concrete.(m.argtypes)),
+                    p.genfn, Tuple(_concrete.(p.argtypes)))
+end
+
+function mh(args::MHKernel...) 
+    return MH(MHKernel[args...])
+end
+
+function is(m::InferenceBundle, p::InferenceBundle)
+    return ISParticle(m.genfn, p.genfn,
+                      Tuple(_concrete.(m.argtypes)),
+                      Tuple(_concrete.(p.argtypes)))
+end
+
+# Not quite used yet.
+struct ObservationSet
+    v::Set{Symbol}
+end
+observe(args::Symbol...) = ObservationSet(Set{Symbol}([args...]))
+
+function loop(kers::Tuple, obs::ObservationSet, n::Int)
+    num_particles = n
+    is_particle = kers[1]
+    return SMC(n, is_particle)
+end
+
+macro infer(expr)
+    @assert(@capture(expr, function prog_(model_, proposal_) 
+                     body__ end))
+    body = map(postwalk.(rmlines ∘ unblock, body)) do e
+        @capture(e, loop(args__) do 
+                     loopbody__ 
+                 end) || return e
+        return quote
+            kers = ($(loopbody...), )
+            loop(kers, $(args...))
+        end
+    end
+    final = quote
+        function $prog(model, proposal)
+            $(body...)
+        end
+    end
+    final = postwalk(rmlines ∘ unblock, final)
+    esc(final)
+end
 
 # Implies SMC -- loop and observe both specified.
-#@infer function infprog(model, proposal)
-#    loop(observe(:x)) do
-#        is(model, proposal)
-#        mh(model, [:y, :z])
-#    end
-#end
-#
-#circuit = infprog(m, sp)
+@infer function infprog(model, proposal)
+    loop(observe(:x), 100) do
+        is(model, proposal)
+        mh(model, proposal)
+    end
+end
+
+circuit = infprog(m, sp)
 
 end # module
