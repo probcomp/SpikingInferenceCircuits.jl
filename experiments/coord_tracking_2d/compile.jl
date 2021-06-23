17fd0aa381d093a92a8ef083f03316ce7aa265e9
George Matheos <georgematheos@gmail.com>
2021-06-23 16:27:47 -0400

    uncomment

--- experiments/coord_tracking_2d/compile.jl
+++ experiments/coord_tracking_2d/compile.jl
@@ -1,32 +1,32 @@
-# using SpikingInferenceCircuits
-# const SIC = SpikingInferenceCircuits
-# using Circuits, SpikingCircuits
-# include("compilable_model.jl")
+using SpikingInferenceCircuits
+const SIC = SpikingInferenceCircuits
+using Circuits, SpikingCircuits
+include("compilable_model.jl")
 
-# latent_domains() = (Positions(), Vels(), Positions(), Vels())
-# obs_domains() = (Positions(), Positions())
-# latent_obs_domains() = (latent_domains()..., obs_domains()...)
-# NPARTICLES() = 4
+latent_domains() = (Positions(), Vels(), Positions(), Vels())
+obs_domains() = (Positions(), Positions())
+latent_obs_domains() = (latent_domains()..., obs_domains()...)
+NPARTICLES() = 4
 
-# rsmcstep = RecurrentSMCStep(
-#     SMCStep(
-#         GenFnWithInputDomains(step_model, latent_domains()),
-#         GenFnWithInputDomains(obs_model, latent_domains()),
-#         GenFnWithInputDomains(step_proposal, latent_obs_domains()),
-#         [:xₜ, :vxₜ, :yₜ, :vyₜ],
-#         [:obsx, :obsy],
-#         NPARTICLES()
-#     ),
-#     [:xₜ, :vxₜ, :yₜ, :vyₜ]
-# )
-# println("SMC Circuit Constructed.")
+rsmcstep = RecurrentSMCStep(
+    SMCStep(
+        GenFnWithInputDomains(step_model, latent_domains()),
+        GenFnWithInputDomains(obs_model, latent_domains()),
+        GenFnWithInputDomains(step_proposal, latent_obs_domains()),
+        [:xₜ, :vxₜ, :yₜ, :vyₜ],
+        [:obsx, :obsy],
+        NPARTICLES()
+    ),
+    [:xₜ, :vxₜ, :yₜ, :vyₜ]
+)
+println("SMC Circuit Constructed.")
 
-# includet("implementation_rules.jl")
-# println("Implementation rules loaded.")
-# impl = Circuits.memoized_implement_deep(rsmcstep, Spiking());
-# println("Circuit implemented deeply.")
+includet("implementation_rules.jl")
+println("Implementation rules loaded.")
+impl = Circuits.memoized_implement_deep(rsmcstep, Spiking());
+println("Circuit implemented deeply.")
 
-# include("../simulation_utils.jl")
+include("../simulation_utils.jl")
 
 inputs = get_smc_circuit_inputs(
     10000, 1000,
