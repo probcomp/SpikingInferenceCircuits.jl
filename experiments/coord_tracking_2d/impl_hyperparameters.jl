67a4b75e9a1eec838e86ab2d5cde54e27518f4c3
George Matheos <georgematheos@gmail.com>
2021-06-23 16:38:58 -0400

    fix hyperparams

--- experiments/coord_tracking_2d/impl_hyperparameters.jl
+++ experiments/coord_tracking_2d/impl_hyperparameters.jl
@@ -1,11 +1,11 @@
-SAMPLE_ONRATE() = 0.2
+SAMPLE_ONRATE() = 0.5
 SCORE_ONRATE() = 0.1
 K() = 20 # count denominator for scores
 
-ΔT() = 500 # ms  -- memory time for scoring unit (also used by many other units)
+ΔT() = 750 # ms  -- memory time for scoring unit (also used by many other units)
 
 MinProb() = 0.1
-ΔT_SAMPLE() = ΔT() * SCORE_ONRATE() / SAMPLE_ONRATE()
+ΔT_SAMPLE() = ΔT() * SAMPLE_ONRATE() / SCORE_ONRATE()
 
 OFFRATE() = 10e-20
 
@@ -19,8 +19,9 @@ M() = 1000 # number of spikes to override off/on gate
 THETA_RATE() = SAMPLE_ONRATE()
 MULT_EXPECTED_OUT_TIME() = ΔT()/2
 MULT_MAX_INPUT_MEMORY() = ΔT_THETA()
+TIMER_N_SPIKES() = 30
 
-SYNC_ΔT_TIMER() = ΔT()/4; NSPIKES_SYNC_TIMER() = N_SPIKES_TIMER()
+SYNC_ΔT_TIMER() = ΔT()/4; NSPIKES_SYNC_TIMER() = TIMER_N_SPIKES()
 SYNC_TIMER_MEMORY() = 3 * SYNC_ΔT_TIMER()
 
 ### Check probabilities of certain types of failure
@@ -40,14 +41,16 @@ bound_on_overall_failure_prob(n_steps, n_vars, n_particles) = 1 - (
 # We need K() spikes to occur from an assembly with rate SCORE_ONRATE() when sampling.
 # So we need P[spikes from P.P. with rate SCORE_ONRATE() in ΔT > K()] > SCORER_FAILUREPROB
 # I.e. P[Poisson(SCORE_ONRATE() * ΔT) > K()] > SCORER_FAILUREPROB
-@assert cdf(Poisson(SCORE_ONRATE() * ΔT), K()) < SCORE_FAIL_PROB() "Probability of not getting enough spikes from scoring unit before memory runs out is too high."
+@assert cdf(Poisson(SCORE_ONRATE() * ΔT()), K()) < SCORE_FAIL_PROB() "Probability of not getting enough spikes from scoring unit before memory runs out is too high."
 
-# We may need K() spikes from a spiker of rate MinProb() * SAMPLE_ONRATE()
-@assert cdf(Poisson(SAMPLE_ONRATE() * MinProb() * ΔT_SAMPLE()), K()) < SAMPLE_FAIL_PROB() "Probability of not getting enough spikes from reciprical scoring unit before memory runs out is too high (at least for the lowest probability outcome)"
+# We may need K() spikes from a spiker of rate SAMPLE_ONRATE()*MinProb()
+@assert cdf(Poisson(SAMPLE_ONRATE()*MinProb()* ΔT_SAMPLE()), K()) < SAMPLE_FAIL_PROB() "Probability of not getting enough spikes from reciprical scoring unit before memory runs out is too high (at least from lowest prob)"
 
+p_mult_output_longer_than(T) = cdf(Erlang(TIMER_N_SPIKES(), 1/timer_rate_for_time(MULT_EXPECTED_OUT_TIME())), T)
+timer_rate_for_time(T) = T/TIMER_N_SPIKES()
 # Want: P[doesn't finish timer before forgetting inputs] ≈ 0
 # ie    P[Erlang(...) > MULT_MAX_INPUT_MEMORY()] ≈ 0
-@assert cdf(Erlang(TIMER_N_SPIKES(), 1/timer_rate_for_time(MULT_EXPECTED_OUT_TIME())), MULT_MAX_INPUT_MEMORY()) < MULT_FAIL_PROB()
+@assert cdf(Erlang(TIMER_N_SPIKES(), 1/timer_rate_for_time(MULT_EXPECTED_OUT_TIME())), MULT_MAX_INPUT_MEMORY()) > 1 - MULT_FAIL_PROB()
 
 # similar check to above, but for reset timer in sync units
-@assert cdf(Erlang(NSPIKES_SYNC_TIMER(), NSPIKES_SYNC_TIMER()/SYNC_ΔT_TIMER()), SYNC_TIMER_MEMORY()) > SYNC_FORGET_FAIL_PROB()
+@assert cdf(Erlang(NSPIKES_SYNC_TIMER(), NSPIKES_SYNC_TIMER()/SYNC_ΔT_TIMER()), SYNC_TIMER_MEMORY()) > 1 - SYNC_FORGET_FAIL_PROB()
