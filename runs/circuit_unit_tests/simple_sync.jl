f1fe4dc8e144e102931999452aa2ee0897a6cbfc
George Matheos <georgematheos@gmail.com>
2021-06-24 17:03:35 -0400

    fix some runs

--- runs/circuit_unit_tests/simple_sync.jl
+++ runs/circuit_unit_tests/simple_sync.jl
@@ -8,9 +8,9 @@ const Sim = SpikingCircuits.SpikingSimulator
 Circuits.implement(s::PulseIR.Sync, ::Spiking) =
     PulseIR.PoissonSync(
         s.cluster_sizes,
-        (1000, 20),
-        (0.1, 1000, 20),
-        (50., 4, (0.1, 1000, 20), 0., 100.)
+        (1000, 0., 5.),
+        (0.1, 0., 5.),
+        (50., 4, (0.1, 1000, 0., 5.), 0., 100.)
     )
 
 sync = PulseIR.Sync([2, 1, 1])
@@ -23,4 +23,8 @@ get_events(impl) = SpikingSimulator.simulate_for_time_and_get_events(impl, 500.;
 events = get_events(impl)
 println("Simulation completed")
 
-include("spiketrain_utils.jl")
+dict = out_st_dict(events)
+@test length(dict) == 3
+for v in values(dict)
+    @test length(v) == 1
+end
