72936c6bd39add04c0bda266a6073d0f056a1b98
George Matheos <georgematheos@gmail.com>
2021-06-28 14:04:18 -0400

    fix hyperparameters for smc

--- experiments/coord_tracking_2d/impl_hyperparameters.jl
+++ experiments/coord_tracking_2d/impl_hyperparameters.jl
@@ -11,8 +11,8 @@ OFFRATE() = 10e-20
 
 # rates for neurons in logic gates like TI, OFFGATE, ASYNC_ON_GATE
 GATE_OFFRATE() = 0.
-GATE_ONRATE() = 5. # KHz
-LOWER_GATE_ONRATE() = 1. # KHz
+GATE_ONRATE() = 200. # KHz
+LOWER_GATE_ONRATE() = 50_000. # KHz -- not realistic at all.  making this realistic: TODO
 GATE_RATES() = (GATE_OFFRATE(), GATE_ONRATE())
 
 M() = 1000 # number of spikes to override off/on gate
@@ -36,15 +36,21 @@ SAMPLE_FAIL_PROB() = 10e-10
 MULT_FAIL_PROB() = 1e-5
 P_FAILURE_MULT_INTO_THETA() = 1e-5
 SYNC_FORGET_FAIL_PROB() = 1e-5
+P_WTA_TOO_SLOW() = 1e-5
 
 bound_on_overall_failure_prob(n_steps, n_vars, n_particles) = 1 - (
-      ((1 - SCORE_FAIL_PROB()) * (1 - SAMPLE_FAIL_PROB()))^n_vars              # prob failure due to sampling/scoring
-    * ((1 - MULT_FAIL_PROB()) * (1 - P_FAILURE_MULT_INTO_THETA()))^n_particles # prob we have a failure on the multiplier or theta
-    * (1 - SYNC_FORGET_FAIL_PROB())                                            # prob failure in sync
+  (    ((1 - SCORE_FAIL_PROB()) * (1 - SAMPLE_FAIL_PROB()) * (1 - P_WTA_TOO_SLOW()))^n_vars # prob failure due to sampling/scoring
+    * (1 - MULT_FAIL_PROB()) * (1 - P_FAILURE_MULT_INTO_THETA())    )^n_particles           # prob we have a failure on the multiplier or theta
+    * (1 - SYNC_FORGET_FAIL_PROB())                                                         # prob failure in sync
 )^n_steps
 
 # Low probability of getting an eroneous spike from a logic gate while it's supposed to be off:
 @assert GATE_OFFRATE() == 0 || error("TODO: implement a check for logic gate being unlikely to spike while off!")
+
+# Low probability of multiple spikes into a WTA before it can output a spike
+# P[exponential(LOWER_GATE_ONRATE()) > exponential(SAMPLE_ONRATE())]
+# = 1 - LOWER_GATE_ONRATE()/(SAMPLE_ONRATE() + LOWER_GATE_ONRATE())
+@assert 1 - (LOWER_GATE_ONRATE()/(SAMPLE_ONRATE() + LOWER_GATE_ONRATE())) < P_WTA_TOO_SLOW() 
 
 # We need K() spikes to occur from an assembly with rate SCORE_ONRATE() when sampling.
 # So we need P[spikes from P.P. with rate SCORE_ONRATE() in ΔT > K()] > SCORER_FAILUREPROB
