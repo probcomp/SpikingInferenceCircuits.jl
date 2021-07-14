8d7674cadf0c234b3d2f074800c93041ee57b9f7
George Matheos <georgematheos@gmail.com>
2021-07-14 12:45:17 -0400

    visualization + rung

--- experiments/tracking_with_occlusion/main.jl
+++ experiments/tracking_with_occlusion/main.jl
@@ -2,6 +2,9 @@ using DynamicModels
 include("model.jl")
 include("groundtruth_rendering.jl")
 include("proposal.jl")
+include("visualize.jl")
+
+ProbEstimates.use_perfect_weights!()
 
 model = @DynamicModel(init_latent_model, step_latent_model, obs_model, 5)
 init_prob = @compile_initial_proposal(_initial_proposal, 1)
@@ -9,7 +12,10 @@ step_prop = @compile_step_proposal(_step_proposal, 5, 1)
 
 @load_generated_functions()
 
-tr = simulate(model, (10,));
+tr, _ = generate(model, (10,), choicemap(
+	(:init => :latents => :xₜ => :val, 1),
+	(:init => :latents => :vxₜ => :val, 2)
+));
 
 obs_choicemap_to_matrix(ch) =
 	[
@@ -23,7 +29,7 @@ unweighted_trs, _ = dynamic_model_smc(
     init_prob, step_prop, 10
 );
 
-nothing
+(fig, t) = draw_gt_and_particles(tr, unweighted_trs); fig
 
 # TODO: enumerate.  Performance will probably be an issue.
 # Maybe using Marco's factor-graph library could help...but getting it set up
@@ -32,4 +38,4 @@ nothing
 # probs = enumeration_bayes_filter_from_groundtruth(
 #     tr, init_latent_model, init_step_model, obs_model,
 #     domains()
-# ) |> DynamicModels.nest_all_addrs_at_val |> collect
+# ) |> DynamicModels.nest_all_addrs_at_val |> collectk
