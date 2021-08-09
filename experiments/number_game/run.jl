34c32c49f2a883f904aa9bbda4bd2b5bbf5ef234
George Matheos <georgematheos@gmail.com>
2021-08-08 20:49:31 -0400

    bug fixes for visualizations

--- experiments/number_game/run.jl
+++ experiments/number_game/run.jl
@@ -73,11 +73,17 @@ function do_enumeration_save_fig(nums)
     save(filename_for_enumeration_run(nums), f)
 end
 
-function do_smc_inference_on_nums_and_save_fig(nums, n_particles, n_pgibbs_particles, n_rejuv_sweeps=1)
+function do_smc_inference_on_nums_and_save_fig(
+    nums, n_particles, n_pgibbs_particles, n_rejuv_sweeps=1;
+    title=get_title(nums, n_particles, n_pgibbs_particles, n_rejuv_sweeps),
+    resolution=(400, 200)
+)
     (_, weighted_trs) = do_smc_inference(trace_with_nums(nums), n_particles, n_pgibbs_particles, n_rejuv_sweeps)
     end_weighted_traces = [(tr, exp(wt)) for (tr, wt) in last(weighted_trs)]
 
-    f = visualize_weighted_traces(end_weighted_traces; title=get_title(nums, n_particles, n_pgibbs_particles, n_rejuv_sweeps))
+    f = visualize_weighted_traces(
+        end_weighted_traces; title, resolution
+    )
     
     save(filename_for_smc_run(nums, n_particles, n_pgibbs_particles, n_rejuv_sweeps), f)
 end
@@ -136,18 +142,23 @@ end
 
 ### Code to do a bunch of runs:
 # specs = list of `(nums, n_particles, n_pgibbs_particles, n_rejuvenation_sweeps_per_timestep)`
-function do_smc_runs(specs)
+function do_smc_runs(specs; override_title_to=nothing)
     for (i, spec) in enumerate(specs)
-    @info "On spec $i / $(length(specs))."
-    try_run() = try
-        do_inference_on_nums_and_save_fig(spec...)
-        true
-    catch e
-        @error "$e"
-        false
-    end
-    while !try_run();
-        println("attempting again...")
+        @info "On spec $i / $(length(specs))."
+        try_run() = try
+            if isnothing(override_title_to)
+                do_smc_inference_on_nums_and_save_fig(spec...)
+            else
+                do_smc_inference_on_nums_and_save_fig(spec...; title=override_title_to)
+            end
+            true
+        catch e
+            @error "$e"
+            false
+        end
+        while !try_run();
+            println("attempting again...")
+        end
     end
 end
 
@@ -155,14 +166,24 @@ end
 
 early_nums = [30, 31, 33, 24, 21, 36, 39]
 late_nums = [30, 33, 24, 21, 36, 31, 39]
+# numss = [
+#     early_nums,
+#     late_nums,
+#     [33, 24, 21, 36, 31, 39, 30],
+#     [24, 21, 36, 31, 39, 30, 33],
+#     [21, 36, 31, 39, 30, 33, 21],
+#     [36, 31, 39, 30, 33, 21, 21]
+# ]
+
 numss = [
-    early_nums,
-    late_nums,
-    [33, 24, 21, 36, 31, 39, 30],
-    [24, 21, 36, 31, 39, 30, 33],
-    [21, 36, 31, 39, 30, 33, 21],
-    [36, 31, 39, 30, 33, 21, 21]
+    late_nums[1:2],
+    late_nums[1:4],
+    late_nums[1:6]
 ]
+specs = [
+    (nums, 100, 2, 2) for nums in numss
+]
+do_smc_runs(specs; override_title_to="")
 
 # specs = Iterators.flatten(
 #     (
@@ -176,5 +197,5 @@ numss = [
 # do_enumeration_save_fig(late_nums)
 
 ### Spiketrain Figure:
-(_, weighted_trs) = do_smc_inference(trace_with_nums(nums), n_particles, n_pgibbs_particles, n_rejuv_sweeps)
-f = make_spiketrain_fig(last(unweighted_trs)[1]; resolution=(600, 450), title="Dynamically Weighted Spike Code from Inference")
+# (_, weighted_trs) = do_smc_inference(trace_with_nums(nums), n_particles, n_pgibbs_particles, n_rejuv_sweeps)
+# f = make_spiketrain_fig(last(unweighted_trs)[1]; resolution=(600, 450), title="Dynamically Weighted Spike Code from Inference")
--- experiments/tracking_with_occlusion/main.jl
+++ experiments/tracking_with_occlusion/main.jl
@@ -1,109 +1,117 @@
-using DynamicModels
-includet("model.jl")
-includet("groundtruth_rendering.jl")
-includet("prior_proposal.jl")
+# using DynamicModels
+# includet("model.jl")
+# includet("groundtruth_rendering.jl")
+# includet("prior_proposal.jl")
 # includet("visualize.jl")
-includet("locally_optimal_proposal.jl")
+# includet("locally_optimal_proposal.jl")
 
-use_ngf() = true
+# use_ngf() = true
 
-if use_ngf()
-    ProbEstimates.use_noisy_weights!()
-else
-    ProbEstimates.use_perfect_weights!()
-end
+# if use_ngf()
+#     ProbEstimates.use_noisy_weights!()
+# else
+#     ProbEstimates.use_perfect_weights!()
+# end
 
-model = @DynamicModel(init_latent_model, step_latent_model, obs_model, 5)
-init_prop = @compile_initial_proposal(_init_proposal, 1)
-step_prop = @compile_step_proposal(_step_proposal, 5, 1)
+# model = @DynamicModel(init_latent_model, step_latent_model, obs_model, 5)
+# init_prop = @compile_initial_proposal(_init_proposal, 1)
+# step_prop = @compile_step_proposal(_step_proposal, 5, 1)
 
-@load_generated_functions()
+# @load_generated_functions()
 
-obs_choicemap_to_vec_of_vec(ch) = [
-    [
-        ch[:img_inner => x => y => :pixel_color => :val]
-        for x=1:ImageSideLength()
-    ]
-    for y=1:ImageSideLength()
-]
+# obs_choicemap_to_vec_of_vec(ch) = [
+#     [
+#         ch[:img_inner => x => y => :pixel_color => :val]
+#         for x=1:ImageSideLength()
+#     ]
+#     for y=1:ImageSideLength()
+# ]
 
-### Run inference:
-do_inference(gt_tr; n_particles=10) = dynamic_model_smc(
-    model, get_dynamic_model_obs(gt_tr),
-    cm -> (obs_choicemap_to_vec_of_vec(cm),),
-    init_prop, step_prop, n_particles
-);
+# ### Run inference:
+# do_inference(gt_tr; n_particles=10) = dynamic_model_smc(
+#     model, get_dynamic_model_obs(gt_tr),
+#     cm -> (obs_choicemap_to_vec_of_vec(cm),),
+#     init_prop, step_prop, n_particles
+# );
 
-function make_gt_particle_viz(gt_tr, unweighted_inferred_trs)
-    GLMakie.activate!()
-    nparticles = length(first(unweighted_trs))
-    draw_gt_and_particles(tr, unweighted_trs,
-    "$nparticles-particle SMC w/ locally-optimal proposal. Run in $(use_ngf() ? "NeuralGen-Fast." : "Vanilla Gen.")"
-    );
-end
+# function make_gt_particle_viz(gt_tr, unweighted_inferred_trs)
+#     GLMakie.activate!()
+#     nparticles = length(first(unweighted_trs))
+#     draw_gt_and_particles(tr, unweighted_trs,
+#     "$nparticles-particle SMC w/ locally-optimal proposal. Run in $(use_ngf() ? "NeuralGen-Fast." : "Vanilla Gen.")"
+#     );
+# end
 
-function surround3(ch, a, dom)
-    v = ch[a => :val]
-    if v-1 in dom && v+1 in dom
-        return (v-1):v+1
-    elseif v-1 in dom && v-2 in dom
-        return (v-2):v
-    else
-        return v:(v+2)
-    end
-end
-latent_domains_for_viz(ch)     = (
-    occₜ = surround3(ch, :occₜ, positions(OccluderLength())),
-    xₜ   = surround3(ch, :xₜ, positions(SquareSideLength())),
-    yₜ   = surround3(ch, :yₜ, positions(SquareSideLength())),
-    vxₜ  = surround3(ch, :vxₜ, Vels()),
-    vyₜ  = surround3(ch, :vyₜ, Vels())
-)
+# function surround3(ch, a, dom)
+#     v = ch[a => :val]
+#     if v-1 in dom && v+1 in dom
+#         return (v-1):v+1
+#     elseif v-1 in dom && v-2 in dom
+#         return (v-2):v
+#     else
+#         return v:(v+2)
+#     end
+# end
+# latent_domains_for_viz(ch)     = (
+#     occₜ = surround3(ch, :occₜ, positions(OccluderLength())),
+#     xₜ   = surround3(ch, :xₜ, positions(SquareSideLength())),
+#     yₜ   = surround3(ch, :yₜ, positions(SquareSideLength())),
+#     vxₜ  = surround3(ch, :vxₜ, Vels()),
+#     vyₜ  = surround3(ch, :vyₜ, Vels())
+# )
 
-function make_spiketrain_fig(tr, neurons_to_show_indices=1:3; nest_all_at, kwargs...)
-    ProbEstimates.Spiketrains.SpiketrainViz.CairoMakie.activate!()
-    propose_sampling_tree = Dict(
-        :occₜ => [], :xₜ => [:occₜ], :yₜ => [],
-        :vxₜ => [:xₜ], :vyₜ => [:yₜ]
-    )
-    assess_sampling_tree = Dict(
-        :occₜ => [], :vxₜ => [], :vyₜ => [],
-        :xₜ => [:occₜ, :vxₜ],
-        :yₜ => [:vyₜ]
-    )
-    propose_addr_topological_order = [:occₜ, :xₜ, :yₜ, :vxₜ, :vyₜ]
+# function make_spiketrain_fig(tr, neurons_to_show_indices=1:3; nest_all_at, kwargs...)
+#     ProbEstimates.Spiketrains.SpiketrainViz.CairoMakie.activate!()
+#     propose_sampling_tree = Dict(
+#         :occₜ => [], :xₜ => [:occₜ], :yₜ => [],
+#         :vxₜ => [:xₜ], :vyₜ => [:yₜ]
+#     )
+#     assess_sampling_tree = Dict(
+#         :occₜ => [], :vxₜ => [], :vyₜ => [],
+#         :xₜ => [:occₜ, :vxₜ],
+#         :yₜ => [:vyₜ]
+#     )
+#     propose_addr_topological_order = [:occₜ, :xₜ, :yₜ, :vxₜ, :vyₜ]
     
-    doms = latent_domains_for_viz(get_submap(get_choices(tr), nest_all_at))
-    return ProbEstimates.Spiketrains.draw_spiketrain_group_fig(
-        ProbEstimates.Spiketrains.value_neuron_scores_groups(keys(doms), values(doms), neurons_to_show_indices), tr,
-        (propose_sampling_tree, assess_sampling_tree, propose_addr_topological_order);
-        nest_all_at, kwargs...
-    )
-end
+#     doms = latent_domains_for_viz(get_submap(get_choices(tr), nest_all_at))
+#     return ProbEstimates.Spiketrains.draw_spiketrain_group_fig(
+#         ProbEstimates.Spiketrains.value_neuron_scores_groups(keys(doms), values(doms), neurons_to_show_indices), tr,
+#         (propose_sampling_tree, assess_sampling_tree, propose_addr_topological_order);
+#         nest_all_at, kwargs...
+#     )
+# end
 
 
-### Generate a particular trace:
-occluded_bounce_constraints() = choicemap(
-	(:init => :latents => :xₜ => :val, 1),
-	(:init => :latents => :vxₜ => :val, 2),
-    (:init => :latents => :occₜ => :val, 8),
-    (:steps => 5 => :latents => :occₜ => :val, 8)
-)
+# ### Generate a particular trace:
+# occluded_bounce_constraints() = choicemap(
+# 	(:init => :latents => :xₜ => :val, 1),
+# 	(:init => :latents => :vxₜ => :val, 2),
+#     (:init => :latents => :occₜ => :val, 8),
+#     (:steps => 5 => :latents => :occₜ => :val, 8)
+# )
 
-generate_occluded_bounce_tr() = generate(model, (15,), occluded_bounce_constraints())
+# generate_occluded_bounce_tr() = generate(model, (15,), occluded_bounce_constraints())[1]
 
 ### Script to run inference + make visualizations
-gt_tr = generate_occluded_bounce_tr()
-(unweighed_trs, _) = do_inference(gt_tr)
+# gt_tr = generate_occluded_bounce_tr()
+# (unweighed_trs, _) = do_inference(gt_tr)
 
 # Inference results animation:
-(fig, t) = make_gt_particle_viz(gt_tr, unweighed_trs); fig
+# (fig, t) = make_gt_particle_viz(gt_tr, unweighed_trs); fig
 
 # Spiketrain figure:
-f = make_spiketrain_fig(
-    last(unweighted_trs)[1], 1:3; nest_all_at=(:steps => 2 => :latents),
-    resolution=(600, 450), figure_title="Dynamically Weighted Spike Code from Inference"
-)
+# f = make_spiketrain_fig(
+#     last(unweighted_trs)[1], 1:3; nest_all_at=(:steps => 2 => :latents),
+#     resolution=(600, 450), figure_title="Dynamically Weighted Spike Code from Inference"
+# )
 
 # Draw observations:
-# (fig, t) = draw_obs(tr); fig
+tr, _= generate(model, (2,), choicemap(
+    (:init => :latents => :xₜ => :val, 6),
+    (:init => :latents => :yₜ => :val, 6),
+    (:init => :latents => :occₜ => :val, 7)
+))
+using CairoMakie
+CairoMakie.activate!()
+(fig, t) = draw_obs(tr); fig
+
