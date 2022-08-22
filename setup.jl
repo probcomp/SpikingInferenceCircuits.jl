a12dd8d278fc7315cfd77235197154d740800330
George Matheos <georgematheos@gmail.com>
2022-08-21 19:10:45 -0700

    improve setup script

--- setup.jl
+++ setup.jl
@@ -6,6 +6,8 @@ macro tryrm(str)
        catch; end; )
 end
 
+Pkg.activate(".")
+
 @tryrm "Circuits"
 @tryrm "SpikingCircuits"
 @tryrm "ANNDistributions"
@@ -13,16 +15,40 @@ end
 @tryrm "CPTs"
 @tryrm "DynamicModels"
 @tryrm "ProbEstimates"
-@tryrm CircuitViz
+@tryrm "CircuitViz"
+
+Pkg.activate("src/ProbEstimates")
+
+@tryrm "Circuits"
+@tryrm "SpikingCircuits"
+@tryrm "ANNDistributions"
+@tryrm "DiscreteIRTransforms"
+@tryrm "CPTs"
+@tryrm "DynamicModels"
+@tryrm "ProbEstimates"
+@tryrm "CircuitViz"
+
+Pkg.develop(path="src/DiscreteIRTransforms")
+Pkg.develop(path="src/DynamicModels")
+
+# Pkg.develop(path="src/CPTs")
+# Pkg.develop(path="src/ANNDistributions")
+
+# Pkg.develop(path="src/DynamicModels")
+# Pkg.develop(path="src/DiscreteIRTransforms/")
+
+Pkg.build()
+
+Pkg.activate(".")
 
 Pkg.add(url="https://github.com/femtomc/CircuitViz.jl")
 Pkg.add(url="git@github.com:probcomp/Circuits.jl.git")
 Pkg.add(url="git@github.com:probcomp/SpikingCircuits.jl.git")
 
-Pkg.develop(path="src/ProbEstimates")
 Pkg.develop(path="src/CPTs")
 Pkg.develop(path="src/DiscreteIRTransforms")
 Pkg.develop(path="src/DynamicModels")
+Pkg.develop(path="src/ProbEstimates")
 Pkg.develop(path="src/ANNDistributions")
 
 Pkg.build()
